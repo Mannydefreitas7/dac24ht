0ec35aa7f932e40e1f619df5305fa32ce23eb6e6
Manny <manny@viulogix.com>
2023-12-22 17:04:02 -0500

    adasd

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const handlebars = require('express-handlebars');
 const app = express()
 const hbs = create({
     layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
     extname: 'hbs'
 
 });
@@ -12,17 +13,16 @@ const port = 8000
 
 
 //Sets our app to use the handlebars engine
-app.set('view engine', 'hbs');
-app.set('views', '../src/views')
-app.use(express.static('../src/public'))
+//app.set('view engine', 'hbs');
+//app.set('views', __dirname + '/views')
+app.use(express.static(__dirname + '/public'))
 //Sets handlebars configurations (we will go through them later on)
-app.engine('hbs', hbs.engine);
+//app.engine('hbs', hbs.engine);
 
 app.get('/', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', {layout: 'index'});
+    res.render('main', {layout: 'index1'});
 });
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
